a41aaf289230be6dcf47f47cce4e680531f1b826
momar07 <bumaxp07@gmail.com>
2026-03-06 14:24:33 +0200

    add iventory module v1-add signal stock alert

--- pos_backend/inventory/apps.py
+++ pos_backend/inventory/apps.py
@@ -4,3 +4,12 @@ class InventoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventory'
     verbose_name = 'إدارة المخزون'
+
+
+
+class InventoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'inventory'
+
+    def ready(self):
+        import inventory.signals  # ← هذا السطر يُفعّل الـ Signals
